c79f758a067d57fd8cc2ec83874f11ccfbd3dac2
Wobins <azalyange19@gmail.com>
2023-05-25 01:35:44 +0100

    Updated navbar with mui avatar, menuicon, and logo

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Avatar } from '@mui/material';
+import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import "./styles.css";
+
+const hostshare_green_logo = require("../../assets/images/Hostshare-logo-green.png") 
 
 const NavigationBar = () => {
     return (
-        <header>
-            <nav id='navbar' className='drop-shadow-md bg-blue-500 text-white py-4 container'>
-                <div className="flex justify-between">
-                    <div id='navbar-logo'>hey</div>
-                    <div id='navbar-search'>hey</div>
-                    <div id='navbar-menu'>hey</div>
+        <header className='py-4 border-b-2 bg-white fixed top-0 left-0 right-0 m-0'>
+            <nav id='navbar' className='m-0 container'>
+                <div className="flex justify-between items-center">
+                    <div id='navbar-logo'>
+                        <Link to="/">
+                            <img src={hostshare_green_logo} alt="Hostshare green logo" className='logo' />
+                        </Link>
+                    </div>
+                    <div id='navbar-search-form'>
+                        <form action="" className='inline-box'>
+                            <input type="text" />
+                            <input type="text" />
+                            <input type="text" />
+                        </form>
+                    </div>
+                    <div id='navbar-menu'>
+                        <div className="flex flex-row items-center border-2 p-2 rounded-full">
+                            <MenuIcon className='m-2' />
+                            <Avatar className='avatar' />
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
