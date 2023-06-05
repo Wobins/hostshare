348fe0ecea265b3557523aa4dc001ce6278941d1
Wobins <azalyange19@gmail.com>
2023-06-05 20:38:37 +0100

    Updated JSON file for listing page

--- src/components/AmenitiesModal/index.jsx
+++ src/components/AmenitiesModal/index.jsx
@@ -3,8 +3,9 @@ import { Modal, Box } from '@mui/material';
 import groups from '../../utils/groups';
 import styles from '../../utils/amenitiesModalStyles';
 
-const BASE_URL = "https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685561688948&signature=-ePEfSK1Pv7q3Lm3d9RoI-ja55ze43sB9ftPrR1m1aI&downloadName=listings.json";
+// const BASE_URL = "https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685561688948&signature=-ePEfSK1Pv7q3Lm3d9RoI-ja55ze43sB9ftPrR1m1aI&downloadName=listings.json";
 // const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = "https://hostshare.s3.amazonaws.com/db.json";
 
 const AmenitiesModal = (props) => {
     const { open, onClose, id } = props;
@@ -23,9 +24,9 @@ const AmenitiesModal = (props) => {
         let listing;
         const res = await fetch(BASE_URL);
         const info = await res.json()
-        // const data = info.data;
-        for (let i = 0; i < info.length; i++) {
-            const el = info[i];
+        const data = info.listings;
+        for (let i = 0; i < data.length; i++) {
+            const el = data[i];
             if (el.info.id === id) {
                 listing = el;
                 break;
--- src/pages/Listing/index.jsx
+++ src/pages/Listing/index.jsx
@@ -6,8 +6,9 @@ import DescriptionModal from '../../components/DescriptionModal';
 import AmenitiesModal from '../../components/AmenitiesModal';
 import truncateText from '../../utils/truncateText'
 
+const BASE_URL = "https://hostshare.s3.amazonaws.com/db.json";
+// const BASE_URL = "https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685561688948&signature=-ePEfSK1Pv7q3Lm3d9RoI-ja55ze43sB9ftPrR1m1aI&downloadName=listings.json";
 // const BASE_URL = process.env.REACT_APP_API_URL;
-const BASE_URL = "https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685561688948&signature=-ePEfSK1Pv7q3Lm3d9RoI-ja55ze43sB9ftPrR1m1aI&downloadName=listings.json";
 
 const Listing = () => {
   let {id} = useParams();
@@ -99,7 +100,7 @@ const Listing = () => {
     let listing;
     const res = await fetch(BASE_URL);
     const info = await res.json()
-    const data = info.data;
+    const data = info.listings;
     for (let i = 0; i < data.length; i++) {
       const el = data[i];
       if (el.info.id === id) {
